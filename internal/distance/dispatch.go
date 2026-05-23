4801a194eaa22828ea46999fb04db64c9ec5172d
POTATO-VE1 <cheezy081106@gmail.com>
2026-05-23 16:37:51 +0530

    update internal/distance/dispatch.go

--- internal/distance/dispatch.go
+++ internal/distance/dispatch.go
@@ -1,18 +1,43 @@
 package distance
 
+import "golang.org/x/sys/cpu"
+
+var useSIMD bool
+
+func init() {
+	useSIMD = cpu.X86.HasAVX2 && cpu.X86.HasFMA
+}
+
 // L2Batch computes L2 squared distance between query and n vectors.
+// Uses AVX2 SIMD on supporting CPUs, falls back to optimized pure Go.
 func L2Batch(query, matrix []float32, n, dim int, results []float32) {
-	L2BatchPure(query, matrix, n, dim, results)
+	if useSIMD {
+		l2BatchSIMD(query, matrix, n, dim, results)
+	} else {
+		L2BatchPure(query, matrix, n, dim, results)
+	}
 }
 
 // DotBatch computes dot product similarity of query with n vectors.
 func DotBatch(query, matrix []float32, n, dim int, results []float32) {
-	DotBatchPure(query, matrix, n, dim, results)
+	if useSIMD {
+		dotBatchSIMD(query, matrix, n, dim, results)
+	} else {
+		DotBatchPure(query, matrix, n, dim, results)
+	}
 }
 
 // CosineBatch computes cosine distance from query to n vectors.
 func CosineBatch(query, matrix []float32, n, dim int, results []float32) {
-	CosineBatchPure(query, matrix, n, dim, results)
+	if useSIMD {
+		var queryNorm float32
+		for j := 0; j < dim; j++ {
+			queryNorm += query[j] * query[j]
+		}
+		cosineBatchSIMD(query, matrix, n, dim, queryNorm, results)
+	} else {
+		CosineBatchPure(query, matrix, n, dim, results)
+	}
 }
 
 // ManhattanBatch computes Manhattan (L1) distance from query to n vectors.
