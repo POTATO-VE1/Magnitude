4aac70ec97c1d3948057e5f4aac307763c29de88
POTATO-VE1 <cheezy081106@gmail.com>
2026-05-23 16:36:54 +0530

    add internal/distance/simd_stub.go

---
+++ internal/distance/simd_stub.go
@@ -0,0 +1,16 @@
+//go:build !amd64
+// +build !amd64
+
+package distance
+
+func l2BatchSIMD(query, matrix []float32, n, dim int, results []float32) {
+	panic("l2BatchSIMD called without SIMD support")
+}
+
+func dotBatchSIMD(query, matrix []float32, n, dim int, results []float32) {
+	panic("dotBatchSIMD called without SIMD support")
+}
+
+func cosineBatchSIMD(query, matrix []float32, n, dim int, queryNorm float32, results []float32) {
+	panic("cosineBatchSIMD called without SIMD support")
+}
